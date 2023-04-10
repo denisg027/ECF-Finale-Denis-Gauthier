e75b15c9be62dc81a8af73f96a9ec58c8d6bc4aa
Retz-o-lution-informatique <79593202+denisg027@users.noreply.github.com>
2023-04-10 15:59:59 +0200

    Création carte menu restaurant

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,3 +10,31 @@ link.addEventListener('click', function(e) {
   burger.classList.toggle('open')
   ul.classList.toggle('open')
 })
+
+
+
+
+const filterContainer = document.querySelector(".gallery-filter");
+const galleryItems = document.querySelectorAll(".gallery-item");
+
+
+filterContainer.addEventListener("click", function (event) {
+		if (event.target.classList.contains("filter-button")) {
+
+			// Désactive les boutons actifs 'filter-button'
+			filterContainer.querySelector(".active").classList.remove("active");
+			// Active les boutons désactivés 'filter-button'
+			event.target.classList.add("active");
+			const filterValue = event.target.getAttribute("data-filter");
+			console.log(filterValue);
+			galleryItems.forEach(function (item) {
+        if (item.classList.contains(filterValue) || filterValue === "all") {
+          item.classList.remove("hide");
+          item.classList.add("show");
+        } else {
+          item.classList.remove("show");
+          item.classList.add("hide");
+        }
+      });
+		}
+	});
