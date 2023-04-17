8e7de7f13359a4c1b9f8925f8ee360644af2b1bb
Retz-o-lution-informatique <79593202+denisg027@users.noreply.github.com>
2023-04-17 13:06:55 +0200

    Mise à jour des fichiers HTML, CSS et JS // Carte Menu Mobil

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,4 @@
-
+/********** Menu Hamburger **********/
 /* Sélection des éléments HTML */
 let link = document.getElementById('link')
 let burger = document.getElementById('burger')
@@ -14,27 +14,3 @@ link.addEventListener('click', function(e) {
 
 
 
-const filterContainer = document.querySelector(".gallery-filter");
-const galleryItems = document.querySelectorAll(".gallery-item");
-
-
-filterContainer.addEventListener("click", function (event) {
-		if (event.target.classList.contains("filter-button")) {
-
-			// Désactive les boutons actifs 'filter-button'
-			filterContainer.querySelector(".active").classList.remove("active");
-			// Active les boutons désactivés 'filter-button'
-			event.target.classList.add("active");
-			const filterValue = event.target.getAttribute("data-filter");
-			console.log(filterValue);
-			galleryItems.forEach(function (item) {
-        if (item.classList.contains(filterValue) || filterValue === "all") {
-          item.classList.remove("hide");
-          item.classList.add("show");
-        } else {
-          item.classList.remove("show");
-          item.classList.add("hide");
-        }
-      });
-		}
-	});
